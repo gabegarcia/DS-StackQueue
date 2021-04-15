d7d1f6ae2d712e3788ce4a53783aa6919b47e981
gabegarcia <gmgarcia1@gmail.com>
2021-04-14 21:31:25 -0700

    Add completed files

--- src/edu/miracosta/cs113/ArrayListStack.java
+++ src/edu/miracosta/cs113/ArrayListStack.java
@@ -13,28 +13,40 @@ import java.util.EmptyStackException;
 import java.util.List;
 
 public class ArrayListStack<E> implements StackInterface<E> {
+	
+	//Data fields
 	public List<E> myList;
+	
+	//Constructor
 	
 	public ArrayListStack() {
 		myList = new ArrayList<E>();
 	}
 	
+	/** Returns boolean true or false if the List is empty
+	 * @return true or false
+	 */
 	@Override
 	public boolean empty() {
-		// TODO Auto-generated method stub
-		
+				
 		return myList.size() == 0;
 	}
 
+	/** Returns the item at the top of the stack without removing it if stack is not empty
+	 * @return item at top of stack; throws EmptyStackException if stack is empty
+	 */
 	@Override
 	public E peek() {
-		// TODO Auto-generated method stub
+		
 		if(empty()) { 
 			throw new EmptyStackException();
 		}
 		return myList.get(myList.size() - 1);
 	}
 
+	/** Returns the item at the top of the stack and removes it if stack is not empty
+	 * @return the item at the top of the stack; throws EmptyStackException if stack is empty
+	 */
 	@Override
 	public E pop() {
 		// TODO Auto-generated method stub
@@ -44,6 +56,10 @@ public class ArrayListStack<E> implements StackInterface<E> {
 		return myList.remove(myList.size() - 1);
 	}
 
+	/** Adds an object to the top of the stack and returns the object
+	 * @param E object to add
+	 * @return returns object
+	 */
 	@Override
 	public E push(E obj) {
 		myList.add(obj);
